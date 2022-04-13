63603a4113f0c3f1d9e5a41c62607c23d41ff136
PeytonSpreacker <peyton@spreacker.com>
2022-04-13 12:33:02 -0500

    initial commit

---
+++ json1.py
@@ -0,0 +1,18 @@
+import json
+
+# JSON data as a dictionary with key/value pairs
+data = '''
+{
+  "name" : "Chuck",
+  "phone" : {
+    "type" : "intl",
+    "number" : "+1 734 303 4456"
+   },
+   "email" : {
+     "hide" : "yes"
+   }
+}'''
+
+info = json.loads(data)
+print('Name:', info["name"])
+print('Hide:', info["email"]["hide"])
---
+++ json2.py
@@ -0,0 +1,22 @@
+import json
+
+# JSON data in the form of a LIST of key/value pairs
+data = '''
+[
+  { "id" : "001",
+    "x" : "2",
+    "name" : "Chuck"
+  } ,
+  { "id" : "009",
+    "x" : "7",
+    "name" : "Brent"
+  }
+]'''
+
+info = json.loads(data)
+print('User count:', len(info))
+
+for item in info:
+    print('Name', item['name'])
+    print('Id', item['id'])
+    print('Attribute', item['x'])
